9e8dbf2c204b9b64ded2c9acb28ddeed9002863b
ksenia <ksenia@bekitzur.com>
2021-06-23 10:05:06 +0300

    Store insert overrides previously inserted data

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -47,19 +47,40 @@ public final class CoreDataFeedStore: FeedStore {
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		context.performAndWait {
-			do {
-				let dtos = try feed.compactMap { try FeedImage(context: context, local: $0) }
-				let _ = try Cache(context: context, images: dtos, timestamp: timestamp)
+			deleteCachedFeed { [weak self] error in
+				guard let self = self else {
+					completion(error)
+					return
+				}
 
-				try context.save()
-				completion(nil)
-			} catch {
-				completion(error)
+				if let error = error {
+					completion(error)
+				} else {
+					do {
+						let dtos = try feed.compactMap { try FeedImage(context: self.context, local: $0) }
+						let _ = try Cache(context: self.context, images: dtos, timestamp: timestamp)
+
+						try self.context.save()
+						completion(nil)
+					} catch {
+						self.context.rollback()
+						completion(error)
+					}
+				}
 			}
 		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		fatalError("Must be implemented")
+		let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Cache")
+		let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+
+		do {
+			try context.execute(deleteRequest)
+			try context.save()
+			completion(nil)
+		} catch {
+			completion(error)
+		}
 	}
 }
